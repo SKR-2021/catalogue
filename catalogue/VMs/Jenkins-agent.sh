f192e628f1dece4dab92b21108d67c9f16db9f50
karunakar2000 <karunakar2000@gmail.com>
2026-02-21 19:40:50 +0200

    code-push

--- catalogue/VMs/Jenkins-agent.sh
+++ catalogue/VMs/Jenkins-agent.sh
@@ -12,16 +12,16 @@ xfs_growfs /home
 
 # This is mandatory, nodejs installation will break SSH if we dont update these packages
 #dnf update -y openssl* openssh* -y
-dnf install java-21-openjdk -y
+sudo dnf install java-21-openjdk -y
 
-dnf module disable nodejs -y
-dnf module enable nodejs:20 -y
-dnf install nodejs -y
+sudo dnf module disable nodejs -y
+sudo dnf module enable nodejs:20 -y
+sudo dnf install nodejs -y
 
 # docker
-yum install -y yum-utils
-yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
-yum install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
-systemctl start docker
-systemctl enable docker
-usermod -aG docker ec2-user
+sudo yum install -y yum-utils
+sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+sudo yum install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+sudo systemctl start docker
+sudo systemctl enable docker
+sudo usermod -aG docker ec2-user
